3219c88362bd11442a7906e7b59e894237348bd5
ildarmgt <ildarmgt@gmail.com>
2018-03-27 12:00:23 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180226194645) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
